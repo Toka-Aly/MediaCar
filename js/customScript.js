76a17f27842a58827b1ab30561c1f6b42dd5d99a
Toka Aly <toaaali72@gmail.com>
2021-04-26 15:26:51 +0200

    first commit

---
+++ js/customScript.js
@@ -0,0 +1,17 @@
+$(window).on('scroll', function() {
+    const scrollValue = $(window).scrollTop();
+
+    if (scrollValue > 140 && scrollValue < 200) {
+        $('header').addClass('scrolled');
+    } else if (scrollValue >= 200) {
+        $('header').addClass('fixed');
+    } else {
+        $('header').removeClass('scrolled').removeClass('fixed');
+    }
+});
+$(document).ready(function() {
+
+    $("#replay").click(function() {
+        $("#replay-field").toggle();
+    });
+});
